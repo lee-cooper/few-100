57e192a4df5e27b47928d97b1ce0d41cf344e61a
Lee Cooper <cooperlee372@gmail.com>
2022-02-21 16:08:06 -0500

    add weather, todo, pokemon, and experiment components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExperimentComponent } from './components/experiment/experiment.component';
 import { HomeComponent } from './components/home/home.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { WeatherComponent } from './components/weather/weather.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'weather',
+    component: WeatherComponent,
+  },
+  {
+    path: 'todo',
+    component: TodoComponent,
+  },
+  {
+    path: 'pokemon',
+    component: PokemonComponent,
+  },
+  {
+    path: 'experiment',
+    component: ExperimentComponent,
   },
   {
     path: '**',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { WeatherComponent } from './components/weather/weather.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { ExperimentComponent } from './components/experiment/experiment.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    WeatherComponent,
+    PokemonComponent,
+    TodoComponent,
+    ExperimentComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
