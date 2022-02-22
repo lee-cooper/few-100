89bc0f719fa3126c7fb4704cc5200f5455e4d3a7
Lee Cooper <cooperlee372@gmail.com>
2022-02-21 20:10:28 -0500

    home title and subtitle. Use string interpolation.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HomeComponent {
+  homeTitle = 'Home';
+  homeSubtitle = 'Select a tile below to begin your journey';
 }
