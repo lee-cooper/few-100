3a11bed9dad427656d148d52babe1cd41ffcd62a
Lee Cooper <cooperlee372@gmail.com>
2022-02-21 20:52:31 -0500

    inspo text, start of pokemon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { TodoComponent } from './components/todo/todo.component';
 import { ExperimentComponent } from './components/experiment/experiment.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { PokemonHeaderComponent } from './components/pokemon/components/pokemon-header/pokemon-header.component';
+import { PokemonDetailComponent } from './components/pokemon/components/pokemon-detail/pokemon-detail.component';
+import { PokemonListComponent } from './components/pokemon/components/pokemon-list/pokemon-list.component';
+import { PokemonService } from './components/pokemon/services/pokemon.service';
 
 @NgModule({
   declarations: [
@@ -21,6 +25,9 @@ import { MatIconModule } from '@angular/material/icon';
     PokemonComponent,
     TodoComponent,
     ExperimentComponent,
+    PokemonHeaderComponent,
+    PokemonDetailComponent,
+    PokemonListComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,7 +35,7 @@ import { MatIconModule } from '@angular/material/icon';
     BrowserAnimationsModule,
     MatIconModule,
   ],
-  providers: [],
+  providers: [PokemonService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
