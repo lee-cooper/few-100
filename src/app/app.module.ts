0fa6dbf9f5c270da24e632aabafe4a7265f37055
Lee Cooper <cooperlee372@gmail.com>
2022-02-27 14:54:55 -0500

    remove pokemon actions, accessibility styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExperimentComponent } from './components/experiment/experiment.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
-import { WeatherComponent } from './components/weather/weather.component';
-import { PokemonComponent } from './components/pokemon/pokemon.component';
-import { TodoComponent } from './components/todo/todo.component';
-import { ExperimentComponent } from './components/experiment/experiment.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
-import { PokemonHeaderComponent } from './components/pokemon/components/pokemon-header/pokemon-header.component';
 import { PokemonDetailComponent } from './components/pokemon/components/pokemon-detail/pokemon-detail.component';
+import { PokemonHeaderComponent } from './components/pokemon/components/pokemon-header/pokemon-header.component';
 import { PokemonListComponent } from './components/pokemon/components/pokemon-list/pokemon-list.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
 import { PokemonService } from './components/pokemon/services/pokemon.service';
-import { HttpClientModule } from '@angular/common/http';
-import { PokemonActionsComponent } from './components/pokemon/components/pokemon-actions/pokemon-actions.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { WeatherComponent } from './components/weather/weather.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +28,6 @@ import { PokemonActionsComponent } from './components/pokemon/components/pokemon
     PokemonHeaderComponent,
     PokemonDetailComponent,
     PokemonListComponent,
-    PokemonActionsComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/pokemon/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon/components/pokemon-list/pokemon-list.component.ts
@@ -1,27 +1,20 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import {
-  PokemonList,
-  PokemonListItem,
-} from '../../models/pokemon-list.interface';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PokemonList, PokemonListItem } from '../../models';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.scss'],
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent {
   @Input() pokemonList!: PokemonList | null;
   @Output() emitSelectedPokemon: EventEmitter<PokemonListItem> =
     new EventEmitter();
 
   selectedPokemon!: PokemonListItem;
 
-  constructor() {}
-
-  ngOnInit(): void {}
-
   selectPokemon(pokemonListItem: PokemonListItem): void {
-    this.selectedPokemon = pokemonListItem; //
+    this.selectedPokemon = pokemonListItem;
     this.emitSelectedPokemon.emit(pokemonListItem);
   }
 }
